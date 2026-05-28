8b8792391edaa8bd99ff7f9601e35d119571f353
Cjoshue18 <joshuacorrea708@gmail.com>
2026-05-28 13:58:36 -0500

    Add basic Authorization method with LoginDTO using Clientes table. This is the first working test.

---
+++ BackEnd/BackEnd/Controllers/AuthController.cs
@@ -0,0 +1,50 @@
+﻿using BackEnd.Data;
+using BackEnd.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : Controller
+    {
+        private readonly FashionShopContext _context;
+
+        public AuthController(FashionShopContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("login")] //añade una subruta url/api/login
+        public async Task<IActionResult> Login([FromBody] LoginDTO loginDto)
+        {
+            if(string.IsNullOrEmpty(loginDto.CliUsername) || string.IsNullOrEmpty(loginDto.CliPasswordHash))
+            {
+                return BadRequest("Ingrese Usuario y Contraseña.");
+            }
+
+            var cliente = await _context.Clientes
+                .FirstOrDefaultAsync(c => c.CliUsername == loginDto.CliUsername);
+
+            if(cliente == null)
+            {
+                return Unauthorized("Usuario o contraseña incorrectos.");
+            }
+
+            if(cliente.CliPasswordHash != loginDto.CliPasswordHash)
+            {
+                return Unauthorized("Usuario o contraseña incorrectos.");
+            }
+
+            return Ok(new
+            {
+                message = "¡Login exitoso",
+                usuario = cliente.CliUsername,
+                nombre = cliente.CliName,
+                token = "test"
+            });
+        }
+
+    }
+}
---
+++ BackEnd/BackEnd/DTOs/LoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BackEnd.DTOs
+{
+    public class LoginDTO
+    {
+        public string CliUsername { get; set; } = string.Empty;
+        public string CliPasswordHash { get; set; } = string.Empty;
+    }
+}
