9047405e53c55863d633d1d919ec0ab1d8801968
Cjoshue18 <joshuacorrea708@gmail.com>
2026-06-23 20:56:21 -0500

    Update backend.http.

--- BackEnd/BackEnd/DTOs/ClientesVista/DireccionesDTO.cs
+++ BackEnd/BackEnd/DTOs/ClientesVista/DireccionesDTO.cs
@@ -2,7 +2,6 @@
 {
     public class DireccionesDTO
     {
-
         public string DirCalle { get; set; } = string.Empty;
         public string DirDistrito { get; set; } = string.Empty;
         public string DirProvincia { get; set; } = string.Empty;
